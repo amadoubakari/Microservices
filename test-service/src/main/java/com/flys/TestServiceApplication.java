7ee5f093633ffaae3542e5d010a5fd2337031da5
amadou <amadou_bakari@yahoo.fr>
2021-01-12 10:24:02 +0400

    refactoring: change the url to keycloack

--- test-service/src/main/java/com/flys/TestServiceApplication.java
+++ test-service/src/main/java/com/flys/TestServiceApplication.java
@@ -12,20 +12,60 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class TestServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestServiceApplication.class, args);
+    }
 
 }
+
 @RefreshScope
 @RestController
 class MessageRestController {
 
-	@Value("${message:Hello default}")
-	private String message;
+    @Value("${message:Hello default}")
+    private String message;
 
-	@RequestMapping("/message")
-	String getMessage() {
-		return this.message;
-	}
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+
+    @RequestMapping("/customer")
+    Customer getCustomer() {
+        return new Customer("CACF","Crédit Agricole Customer Finance");
+    }
+
+    class Customer {
+        String name;
+        String desc;
+
+        public Customer() {
+        }
+
+        public Customer(String name, String desc) {
+            this.name = name;
+            this.desc = desc;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+
+        @Override
+        public String toString() {
+            return "Customer{}";
+        }
+    }
 }
